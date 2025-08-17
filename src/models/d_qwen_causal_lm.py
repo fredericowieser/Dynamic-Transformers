79a981b7f4035eb3fadd79603319c5b1681a0802
Frederico Wieser <20886677+fredericowieser@users.noreply.github.com>
2025-08-17 22:35:54 +0100

    Change Qwen Model Into Decisive Dynamic Transformer Model

--- src/models/d_qwen_causal_lm.py
+++ src/models/d_qwen_causal_lm.py
@@ -2,6 +2,7 @@
 
 import logging
 import torch
+import torch.nn as nn
 from transformers import Qwen2ForCausalLM
 from transformers.modeling_outputs import CausalLMOutputWithPast
 from src.models.d_qwen_config import DynamicQwenConfig
