587820c69afcbbe582486552db44e81c777e7ec1
Frederico Wieser <20886677+fredericowieser@users.noreply.github.com>
2026-03-28 05:42:02 +0000

    Fix eval

--- src/models/sdt/model.py
+++ src/models/sdt/model.py
@@ -271,14 +271,14 @@ class SDTForCausalLM(BaseForCausalLM):
                     hidden_states = torch.utils.checkpoint.checkpoint(
                         layer.__call__,
                         hidden_states,
-                        attn_mask,
-                        position_ids,
-                        past_key_values,
-                        output_attentions,
-                        use_cache,
-                        cache_position,
-                        position_embeddings,
                         use_reentrant=False,
+                        attention_mask=attn_mask,
+                        position_ids=position_ids,
+                        past_key_values=past_key_values,
+                        output_attentions=output_attentions,
+                        use_cache=use_cache,
+                        cache_position=cache_position,
+                        position_embeddings=position_embeddings,
                     )
                 else:
                     layer_outputs = layer(
